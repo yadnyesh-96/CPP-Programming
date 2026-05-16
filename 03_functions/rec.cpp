ccf1f2c8d02d4a4a9709dc7e5a37918466f99c7a
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-05-16 23:16:59 +0530

    square function

---
+++ 03_functions/rec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int getSquare(int num){
+    return num*num;
+}
+
+int getFac(int num){
+    
+}
+
+int main(){
+
+    int n = 5;
+
+    std::cout<< n << " square is :" << getSquare(n) <<std::endl;
+
+    return 0;
+}
