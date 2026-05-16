61dae1dedd8c9fed481cfa5ea742c56c0adcf35f
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-05-16 23:53:03 +0530

    pointers in c++

--- 04_pointers/pointer.cpp
+++ 04_pointers/pointer.cpp
@@ -11,5 +11,8 @@ int main(){
     std::cout<<"By *ptr: = "<<*ptr <<std::endl; //5
     *ptr = 10;
     std::cout<<"(*ptr =10 ) After modify By *ptr: = "<<*ptr <<std::endl;
+
+    int** t = &ptr;
+    **t = 45;
     return 0;
 }
