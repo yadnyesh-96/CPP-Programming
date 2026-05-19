4cee529b539eab8c6184c757a02b3998fb666acd
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-05-19 23:46:39 +0530

    ControlFlows and loops practice

---
+++ 05_ControlFlowsANDloops/compNum.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int n1=3, n2=6;
+
+    if(n1=n2){
+        printf("%d ,%d",n1,n2);
+    }else{
+        printf("%d != %d",n1,n2);
+    }
+
+    return 0;
+}
